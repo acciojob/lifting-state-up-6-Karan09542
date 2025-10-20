198cbddafeff24e6cde8ce0fa036996d616a9811
Karan09542 <karan09542@gmail.com>
2025-10-20 07:19:22 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,35 @@
+import React, { useState } from "react";
+import "./../styles/App.css";
 
-import React from "react";
-import './../styles/App.css';
-
+const TodoList = ({ task, isCompleted, updateTodo, idx }) => {
+  return (
+    <li>
+      {task} {!isCompleted && <button onClick={() => updateTodo(idx)}>Completed</button>}
+    </li>
+  );
+};
 const App = () => {
+  const [todos, setTodos] = useState([
+    { task: "Learn React", isCompleted: false },
+    { task: "Build a React app", isCompleted: false },
+    { task: "Deploy the React app", isCompleted: false },
+  ]);
+
+  function handleCompleted(id) {
+    const updateTodos = [...todos];
+    updateTodos[id].isCompleted = true;
+    setTodos(updateTodos);
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <ul>
+        {todos.map((todo, idx) => (
+          <TodoList {...todo} updateTodo={handleCompleted} idx={idx} />
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
